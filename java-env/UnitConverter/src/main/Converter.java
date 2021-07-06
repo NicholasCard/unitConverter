4500d587cf0449dac5bd2d1d5cfc49994d25a59c
nick <nicholas.card@revature.net>
2021-07-06 00:22:32 -0400

    Completed unitConverter Project minus some of the improvements that I do want to make later

--- java-env/UnitConverter/src/main/Converter.java
+++ java-env/UnitConverter/src/main/Converter.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 public class Converter {
 	
 	
+	public static double cupsToTeaspoons(int cups) {
+	       double result = 0;
+	       result = cups * 48;
+	       return result;
+	}
+	
+	public static double milesToKilometers(int miles) {
+	       double result = 0;
+	       result = miles * 1.60934;
+	       return result;
+	}
+	
+	public static double fToC(int f) {
+	       double result = 0;
+	       result = ((f - 32) * 5/9);
+	       return result;
+	}
 
 	public static void main(String[] args) {
 		
@@ -11,6 +28,7 @@ public class Converter {
 		
 		int menuSelection= 0;
 		
+		 
 		while (true) {
 			
 		
@@ -22,16 +40,49 @@ public class Converter {
 		
 				menuSelection = input.nextInt();
 				
+				
+				/*not sure if its best to do this in the methods themselves or in the case
+				 * seems best to do it in the case because the scanner has to be the same throughout
+				 * 
+				 * it seems like I can make a function for the scanner specifically but this seems to be the best way
+				 * to get specific responses from each conversion
+				 * 
+				 * I do want to Complete the other suggested tasks but I want to make sure
+				 * 	1. I used the access modifiers correctly
+				 *  2. Created and called the functions the right way
+				 *  	-we went over method declaration and I'm not quite sure if that applies to this
+				 *  3. any other questions that come up.
+				 *  
+				 * Hopefully We get feedback on these and if not I can ask tomorrow
+				 * 
+				 */
 				switch(menuSelection) {
 				case 1:
-					System.out.println("Hi im the first one!");
-					break;
+					int cups;
+					System.out.println("\nPlease Enter The Cups: ");
+			        cups = input.nextInt();
+			        
+			        System.out.println("There are " + cupsToTeaspoons(cups) + " Teaspoons in " + cups + " Cups");
+			        
+			        break;
+			        
 				case 2:
-					System.out.println("Hi im the second one!");
+					int miles;
+					System.out.println("\nPlease Enter The Miles: ");
+			        miles = input.nextInt();
+			        
+			        System.out.println("There are " + milesToKilometers(miles) + " Kilometers in " + miles + " Miles");
+					
 					break;
+					
 				case 3:
-					System.out.println("Hi im the Third one!");
-					break;
+					int f;
+					System.out.println("\nPlease Enter The Degrees Fahrenheit: ");
+			        f = input.nextInt();
+			        
+			        System.out.println(f + " Degrees Fahrenheit is " + fToC(f) + " Celsius ");
+					
+			        break;
 					
 				case 4:
 					System.out.println("Hi im the quit one!");
