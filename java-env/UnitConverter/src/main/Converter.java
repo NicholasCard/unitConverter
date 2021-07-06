62a6759830e6d9affdb49eed9652ec903a63a967
nick <nicholas.card@revature.net>
2021-07-05 23:45:59 -0400

    added the switch case and the while loop

--- java-env/UnitConverter/src/main/Converter.java
+++ java-env/UnitConverter/src/main/Converter.java
@@ -1,8 +1,43 @@
 package main;
+import java.util.Scanner;
 
 public class Converter {
+	
+	
 
 	public static void main(String[] args) {
-		System.out.println("This is the start of the converter");
+		
+		Scanner input = new Scanner(System.in);
+		
+		int menuSelection= 0;
+		
+		while (true) {
+			
+		
+		System.out.println("Please Select an option: \n"
+				+ "1. Cups to Teaspoons \n"
+				+ "2. Miles to Kilometers \n"
+				+ "3. Fahrenhiet to Celsius \n"
+				+ "4. Quit");
+		
+				menuSelection = input.nextInt();
+				
+				switch(menuSelection) {
+				case 1:
+					System.out.println("Hi im the first one!");
+					break;
+				case 2:
+					System.out.println("Hi im the second one!");
+					break;
+				case 3:
+					System.out.println("Hi im the Third one!");
+					break;
+					
+				case 4:
+					System.out.println("Hi im the quit one!");
+					break;
+			
+			}
+		}
 	}
 }
